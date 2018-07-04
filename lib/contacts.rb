3f43e8a5f779fe1b3e25fc80de73d48b05716e27
Ana Bebic <anabebic.x@gmail.com>
2018-07-04 11:17:02 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -20,6 +20,7 @@ def remove_strawberry(contacts)
       |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        ice_cream == "strawberry"}
     binding.pry
 end
 
